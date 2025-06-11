cbccb68dcac32bb8b1ed900f8728e3a504910bdf
e-hua <eclipsehua@gmail.com>
2025-06-11 12:03:39 +0800

    feat(frontend): Add Search box in the search landing page with underline input and focus highlight

--- frontend-vite/src/pages/CoursesPage.tsx
+++ frontend-vite/src/pages/CoursesPage.tsx
@@ -1,9 +1,14 @@
+import { Command, CommandInput } from "@/components/diy-ui/UnderlineCommand";
 import Layout from "@/components/Sidebar/layout";
 
 export default function CoursesPage() {
   return (
     <Layout>
-      <></>
+      <div className="p-4">
+        <Command>
+          <CommandInput />
+        </Command>
+      </div>
     </Layout>
   );
 }
