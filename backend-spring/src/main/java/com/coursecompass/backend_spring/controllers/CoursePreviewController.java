230c33b9f220641be7397868dc6622e90b7d97dd
e-hua <eclipsehua@gmail.com>
2025-06-12 21:40:55 +0800

    feat(backend): Implement pageable endpoint for CoursePreviewDTOs

--- backend-spring/src/main/java/com/coursecompass/backend_spring/controllers/CoursePreviewController.java
+++ backend-spring/src/main/java/com/coursecompass/backend_spring/controllers/CoursePreviewController.java
@@ -1,13 +1,11 @@
 package com.coursecompass.backend_spring.controllers;
 
 import com.coursecompass.backend_spring.dto.CoursePreviewDTO;
+import com.coursecompass.backend_spring.dto.PageDTO;
 import com.coursecompass.backend_spring.services.CoursePreviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/coursePreviews")
@@ -24,5 +22,14 @@ public class CoursePreviewController {
   public ResponseEntity<CoursePreviewDTO> getCoursePreview(@PathVariable String courseCode) {
     CoursePreviewDTO coursePreviewDTO = coursePreviewService.fetchModuleFromNUSMods(courseCode);
     return ResponseEntity.ok(coursePreviewDTO);
+  }
+
+  @GetMapping
+  public ResponseEntity<PageDTO<CoursePreviewDTO>> getAllCoursePreviews(
+          @RequestParam(defaultValue = "0") int page,
+          @RequestParam(defaultValue = "20") int size
+  ) {
+    PageDTO<CoursePreviewDTO> result = coursePreviewService.getPaginatedCoursePreviews(page, size);
+    return ResponseEntity.ok(result);
   }
 }
--- backend-spring/src/main/java/com/coursecompass/backend_spring/services/CoursePreviewService.java
+++ backend-spring/src/main/java/com/coursecompass/backend_spring/services/CoursePreviewService.java
@@ -2,7 +2,9 @@ package com.coursecompass.backend_spring.services;
 
 import com.coursecompass.backend_spring.dto.CoursePreviewDTO;
 import com.coursecompass.backend_spring.dto.NUSModsDTO;
+import com.coursecompass.backend_spring.dto.PageDTO;
 
 public interface CoursePreviewService {
    CoursePreviewDTO fetchModuleFromNUSMods(String moduleCode);
+   PageDTO<CoursePreviewDTO> getPaginatedCoursePreviews(int page, int size);
 }
