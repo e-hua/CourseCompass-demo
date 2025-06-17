fe01ad14646903564ffad93079e1f5132ca102e2
e-hua <eclipsehua@gmail.com>
2025-06-17 15:24:01 +0800

    feat(frontend): Add API functions for creating and fetching comments

---
+++ frontend-vite/src/apis/CommentAPI.ts
@@ -0,0 +1,50 @@
+const API_URL = "http://localhost:8080/api/";
+// const API_URL = "https://coursecompass-demo.onrender.com/api/";
+
+export interface CommentCreateDTO {
+  takenCourseId: number;
+  content: string;
+}
+
+export interface CommentReadDTO {
+  // id: number;
+  content: string;
+  authorUsername: string;
+  courseCode: string;
+  letterGrade: string;
+  difficulty: number;
+  averageWorkload: number;
+  enjoyability: number;
+  createdAt: string;
+}
+
+export async function createComment(
+  data: CommentCreateDTO
+): Promise<CommentReadDTO> {
+  const token = localStorage.getItem("id_token");
+  if (!token) throw new Error("Missing ID token");
+
+  const res = await fetch(API_URL + "comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) throw new Error("Failed to create comment: " + res.text());
+  return res.json();
+}
+
+export async function readComments(
+  courseCode: string
+): Promise<CommentReadDTO[]> {
+  const res = await fetch(API_URL + `comments/${courseCode}`, {
+    method: "GET",
+  });
+
+  if (!res.ok) throw new Error("Failed to read comments: " + res.text());
+
+  return res.json();
+}
