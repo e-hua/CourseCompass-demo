64f2d6550a077ac8f3e71d47e29303665397ad8e
Gao Huiying <e1481825@u.nus.edu>
2025-06-27 20:06:55 +0800

    Reapply "solved consistency error"
    This reverts commit f39cff0806b7eb174fa0d8fa3a35c237025d598b.

--- frontend-vite/src/apis/NodesandEdgesAPI.ts
+++ frontend-vite/src/apis/NodesandEdgesAPI.ts
@@ -25,16 +25,16 @@ export async function putPlan(nodes: Node[], edges: Edge[]): Promise<void> {
       Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
-      nodesJson: nodes.map((node) => ({
+      nodesJson: JSON.stringify(nodes.map((node) => ({
         id: node.id,
         position: node.position,
         type: node.type,
-      })),
-      edgesJson: edges.map((edge) => ({
+      }))),
+      edgesJson: JSON.stringify(edges.map((edge) => ({
         source: edge.source,
         target: edge.target,
         id: edge.id,
-      })),
+      }))),
     }),
   });
 
